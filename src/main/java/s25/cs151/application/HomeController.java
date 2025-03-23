5da7e0018061b18a955c869e87f966e005afbba0
kylechu8092 <kc5548092@gmail.com>
2025-03-22 23:22:49 -0700

    Office hour display sorted

--- src/main/java/s25/cs151/application/HomeController.java
+++ src/main/java/s25/cs151/application/HomeController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
 
 public class HomeController {
 
@@ -14,4 +16,17 @@ public class HomeController {
         Scene officeHoursScene = Main.OfficeHoursScene(stage);
         stage.setScene(officeHoursScene);
     }
+
+    @FXML
+    private void handleViewStoredOfficeHours(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/s25/cs151/application/OfficeHoursPage.fxml"));
+            Scene scene = new Scene(loader.load());
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
